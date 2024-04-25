8b80dc642bc911c6ea991a6461e7e699caae1de5
Andriasamsonidze <andriasamsonidze@gmail.com>
2024-04-25 15:45:31 +0400

    Day-06

--- Day-06/classwork/classwork.py
+++ Day-06/classwork/classwork.py
@@ -1,6 +1,5 @@
 user_name = input("what is your name?:  ")
 user_surname = input("what is your surname?:  ")
 user_age = input("how old are you?:  ")
-user_addres = input()
 print(" your name is " + user_name + " your surname is " + user_surname  + " and your age is " + user_age)
 
