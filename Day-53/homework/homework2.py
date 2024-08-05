e5f45cf058cba0cadfb18936e2b48aadebfd4a14
Andriasamsonidze <andriasamsonidze@gmail.com>
2024-08-05 21:41:58 +0400

    commit

--- Day-53/homework/homework2.py
+++ Day-53/homework/homework2.py
@@ -1,2 +1,10 @@
 #  2) შექმენით ფუნქცია რომელშიც იქნება რიცხვებით სავსე სია და ფუნქცია დააბრუნებს ამ რიცხვების ჯამს
 
+def sum(): 
+    sum = 0
+    numbers = [1,5,3,6,7,8,3,67,2,6]
+    for i in range (len(numbers)):
+        sum = sum + numbers[i]
+    print("The sum of numbers is " + str(sum))
+
+sum()
