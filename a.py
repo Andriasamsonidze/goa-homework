e18a58761825590c0e067dc51b947937d4a5e395
Andriasamsonidze <andriasamsonidze@gmail.com>
2024-09-28 00:46:09 +0400

    commit:

--- a.py
+++
@@ -1,8 +0,0 @@
-def square_digits():
-    num = input("Enter an integer: ")
-    result = ""
-    for digit in num:
-        squared_digit = int(digit) ** 2
-        result += str(squared_digit)
-    return int(result)
-print(square_digits())
